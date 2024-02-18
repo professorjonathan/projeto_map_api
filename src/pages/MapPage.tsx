d88f7535bac2b5aaa140c4de5a0695f47333715e
Jonathan Melo <jonathan.melo@jwss.com.br>
2024-02-18 11:29:09 -0300

    initial commit

---
+++ src/pages/MapPage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import './MapPage.css';
+
+export interface MapPageProps { }
+
+const MapPage = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBpp6_iEZdVWE718SDrUBSfW9Qdj9N5MVQ"
+    })
+
+    const position = {
+        lat: -29.966174,
+        lng: -50.117741
+    }
+
+    return (
+        <div className="map">
+            {isLoaded ? (
+                <GoogleMap
+                    mapContainerStyle={{ width: '100%', height: '100%' }}
+                    center={position}
+                    zoom={15}
+                >
+                    <Marker position={position} options={{
+                        label: {
+                            text: "Morando na Praia",
+                            className: "map-position"
+                        }
+                    }}
+                    />
+                </GoogleMap>
+            ) : <></>}
+        </div>
+    )
+}
+
+export default MapPage
